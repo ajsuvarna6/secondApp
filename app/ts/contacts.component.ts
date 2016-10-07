a9a343d1ae093323f5b059d6411f8d4516fd481c
ajsuvarna6 <ajsuvarna6@github.com>
2016-10-07 14:24:17 +0530

    saving changes

--- app/ts/contacts.component.ts
+++ app/ts/contacts.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from './contact';
 import { StorageService } from './storage.service';
-//import * as $ from '@jquery';
+declare var $:any;
+declare var gapi:any;
 
 @Component({
   // moduleId: module.id,
@@ -12,23 +13,72 @@ import { StorageService } from './storage.service';
 export class ContactsComponent implements OnInit {
   allContacts=[];
   selectContact={};
+
   constructor(private storageService: StorageService) {
+    this.googleScript();
+    console.log();
+    this.sample();
+  }
+
+  sample():any {
+    $('document').ready(function(){
+      $('.sampleClass').click(function(){
+        console.log("Clicked button");
+        alert("Clicked");
+        console.log('ddd',$('.col-xs-12'));
+      });
+    });
+  }
+
+  googleScript():void {
+      var google = document.createElement('script');
+      google.type = 'text/javascript';
+      google.async = true;
+      google.src = '//apis.google.com/js/platform.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(google, s);
+      console.log("script added");
+      var that=this;
+      setTimeout(function(){ that.googleApi(); },3000);
+
+  }
+
+  googleApi():void {
+    console.log("came inside");
+    gapi.load('auth2', function() {
+      gapi.auth2.init({
+        client_id: "704173116896-ri8f7pifm59tpuplapmjb8fti5gm3jkd.apps.googleusercontent.com",
+      });
+    });
+    gapi.signin2.render('g-signin2', {
+        'scope': 'profile email https://www.googleapis.com/auth/contacts.readonly',
+        'width': 200,
+        'height': 40,
+        'longtitle': true,
+        'onsuccess': this.onSignIn
+    });
+  }
+
+  onSignIn(googleUser):any {
+    var auth2 = gapi.auth2.getAuthInstance();
+    var profile="";
+    if (auth2.isSignedIn.get()) {
+      profile = auth2.currentUser.get().getBasicProfile();
+    } else {
+      profile = googleUser.getBasicProfile();
+    }
+    //console.log(profile);
+    //var id_token = googleUser.getAuthResponse().id_token;
+        //console.log("ID Token: " + id_token);
   }
 
   ngOnInit():any {
     //this.allContacts=this.storageService.getContacts();
     this.storageService.getContacts().then( data => { this.allContacts=data; this.selectContact=data[0]; } );
     //this.allContacts=data; this.selectContact=this.allContacts[0];
-    var form=new FormData();
-    var el=document.getElementsByClassName('list-group');
-    console.log(form,el);//,jQuery('.list-group'));
   }
 
   onSelect(cont:Contact) {
     this.selectContact=cont;
   }
 }
-
-(function getData(){
-  console.log(document.getElementsByClassName('list-group'));
-})();
